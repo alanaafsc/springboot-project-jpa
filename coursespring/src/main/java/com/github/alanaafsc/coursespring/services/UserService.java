bc32eacd10bc4ae06c22771b9a3a467dabdde94d
alanaafsc <alana.ingrid@ufpe.br>
2022-11-28 10:34:30 -0300

    Service layer, component registration

---
+++ coursespring/src/main/java/com/github/alanaafsc/coursespring/services/UserService.java
@@ -0,0 +1,27 @@
+package com.github.alanaafsc.coursespring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.alanaafsc.coursespring.entities.User;
+import com.github.alanaafsc.coursespring.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
