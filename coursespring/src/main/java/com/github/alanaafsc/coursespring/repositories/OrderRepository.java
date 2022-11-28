ef3f27fb2c4011dcb1ae04f460da56c660ec74e8
alanaafsc <alana.ingrid@ufpe.br>
2022-11-28 11:50:31 -0300

    Update OrderRepository.java

--- coursespring/src/main/java/com/github/alanaafsc/coursespring/repositories/OrderRepository.java
+++ coursespring/src/main/java/com/github/alanaafsc/coursespring/repositories/OrderRepository.java
@@ -3,6 +3,7 @@ package com.github.alanaafsc.coursespring.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.github.alanaafsc.coursespring.entities.Order;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
 }
