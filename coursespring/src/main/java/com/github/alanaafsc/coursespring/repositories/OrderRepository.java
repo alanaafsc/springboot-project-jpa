dfa8386973e558dfd429eb0ac1763bc776e2e27e
alanaafsc <alana.ingrid@ufpe.br>
2022-11-28 11:50:21 -0300

    Order, Instant, ISO 8601 pt 2

---
+++ coursespring/src/main/java/com/github/alanaafsc/coursespring/repositories/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.github.alanaafsc.coursespring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.github.alanaafsc.coursespring.entities.Order;
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
