6fec7d622c9f8db39d0f153de06c198455af99f7
alanaafsc <alana.ingrid@ufpe.br>
2022-11-29 10:53:50 -0300

    Category - entity

---
+++ coursespring/src/main/java/com/github/alanaafsc/coursespring/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.github.alanaafsc.coursespring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.alanaafsc.coursespring.entities.Category;
+import com.github.alanaafsc.coursespring.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
