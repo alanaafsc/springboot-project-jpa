6be83e31a4e6897375efd2b0e8fad6dc9fe4e936
alanaafsc <alana.ingrid@ufpe.br>
2022-12-01 10:16:48 -0300

    Exception handling - findById

--- coursespring/src/main/java/com/github/alanaafsc/coursespring/services/UserService.java
+++ coursespring/src/main/java/com/github/alanaafsc/coursespring/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.github.alanaafsc.coursespring.entities.User;
 import com.github.alanaafsc.coursespring.repositories.UserRepository;
+import com.github.alanaafsc.coursespring.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
