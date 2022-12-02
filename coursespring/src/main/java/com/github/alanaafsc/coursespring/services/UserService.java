29e0f1fd58fd45a4af7282e16ee467f301829617
alanaafsc <alana.ingrid@ufpe.br>
2022-12-02 10:29:38 -0300

    Exception handling - update

--- coursespring/src/main/java/com/github/alanaafsc/coursespring/services/UserService.java
+++ coursespring/src/main/java/com/github/alanaafsc/coursespring/services/UserService.java
@@ -13,6 +13,8 @@ import com.github.alanaafsc.coursespring.repositories.UserRepository;
 import com.github.alanaafsc.coursespring.services.exceptions.DatabaseException;
 import com.github.alanaafsc.coursespring.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
-		return repository.save(entity);
+		return repository.save(entity); 
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
